672bc0cc2bc57cbb556441533c6718a9f11c3a51
sarai2709 <saraicardonad@ielacandelariamedellin.edu.co>
2026-03-17 21:04:42 +0000

    deme un 5

---
+++ 17-03/adivinanza.js
@@ -0,0 +1,10 @@
+let a=90;
+let b=70;
+if(a==b){
+    console.log("Adivinaste");
+}else if(a>b){
+    console.log("muy grande");
+}
+else if(a<b){ 
+    console.log("muy pequeño");
+}
---
+++ 17-03/ciclos.js
@@ -0,0 +1,16 @@
+let numero=1
+while (numero <=100){
+    console.log(numero);
+    numero++;
+}
+
+let num=2;
+while (num <=100){
+    console.log(num);
+    num+=2;
+}
+let num2=1;
+while (num2 <=100){
+    console.log(num2);
+    num2+=2;
+}
